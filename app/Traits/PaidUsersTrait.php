27763d3767305e9e41e3b568cc5bdeaac5c72e6c
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-02-19 16:09:37 +0530

    PaidUsersTrait

---
+++ app/Traits/PaidUsersTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Payment;
+use App\Models\SpoteLight;
+use Carbon\Carbon;
+
+
+trait PaidUsersTrait
+{
+    public function paidUsers()
+    {
+        $payments = Payment::with('user.spotelights', 'plan')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        $paidUsers = [];
+        $userIds = [];
+
+        foreach ($payments as $payment) {
+            $paidPaymentDate = Carbon::parse($payment->expiry_date);
+            $currentDate = Carbon::now('Asia/Kolkata');
+            if ($paidPaymentDate >= $currentDate) {
+                if (!in_array($payment->user_id, $userIds)) {
+                    $paidUsers[] = $payment;
+                    $userIds[] = $payment->user_id;
+                }
+            } else {
+                $payment->update([
+                    'contact' => null,
+                    'is_paid' => 0
+                ]);
+            }
+        }
+
+        return $paidUsers;
+    }
+}
